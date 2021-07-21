dcdd85cf3978a7d3201517542d7627bb63fa1c3b
parkchangwoo1 <wkwmdsk808@ajou.ac.kr>
2021-07-21 23:26:00 +0900

    add: Weather.js (날씨 component 추가) feat: App.js (API 및 axios, Weather.js) package: axios, prop-types

--- App.js
+++ App.js
@@ -2,18 +2,28 @@ import React from "react";
 import { Alert } from "react-native";
 import Loading from "./Loading";
 import * as Location from "expo-location";
+import axios from "axios";
+import Weather from "./Weather";
 
+const API_KEY = "17fbead52099713a933a90e88d9eb5ad";
 export default class extends React.Component {
   state = {
     isLoading: true,
   };
+  getWeather = async (latitude, longitude) => {
+    const { data } = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}&units=metric`
+    );
+    this.setState({ isLoading: false, temp: data.main.temp });
+  };
+
   getLocation = async () => {
     try {
       await Location.requestForegroundPermissionsAsync();
       const {
         coords: { latitude, longitude },
       } = await Location.getCurrentPositionAsync();
-      this.setState({ isLoading: false });
+      this.getWeather(latitude, longitude);
     } catch (error) {
       Alert.alert("Can't find you.", "So sad");
     }
@@ -22,7 +32,7 @@ export default class extends React.Component {
     this.getLocation();
   }
   render() {
-    const { isLoading } = this.state;
-    return isLoading ? <Loading /> : null;
+    const { isLoading, temp } = this.state;
+    return isLoading ? <Loading /> : <Weather temp={Math.round(temp)} />;
   }
 }
